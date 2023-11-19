48a412e1131a943d806cf2c13318f5d8c88c77b2
vi.channnnn <violet@himaa.me>
2023-11-19 13:28:09 +0800

    feat: update subtitle to fade in instead of typing (reason: annoying)

--- src/features/Hero/Hero.tsx
+++ src/features/Hero/Hero.tsx
@@ -1,4 +1,5 @@
 import { TypeAnimation } from 'react-type-animation';
+import FadeIn from 'react-fade-in';
 import './Hero.css';
 
 export const Hero = () => {
@@ -14,32 +15,22 @@ export const Hero = () => {
           />
         </h1>
         <p className='hero-subtitle'>
-          <TypeAnimation
-            style={{ whiteSpace: 'pre-line', display: 'block' }}
-            sequence={[
-              "Yahallo~ I'm Hima, currently employed full-time as a Full-Stack Software Engineer in Singapore.",
-            ]}
-            speed={99}
-            cursor={false}
-          />
+          <FadeIn>
+            Yahallo~ I'm Hima, currently employed full-time as a Full-Stack Software Engineer in
+            Singapore.
+          </FadeIn>
           <br></br>
-          <TypeAnimation
-            style={{ whiteSpace: 'pre-line', display: 'block' }}
-            sequence={[
-              'I was previously a Game Designer at a local game company developing JRPG anime-style mobile games for two years before transitioning into Software Engineering as a freelance Backend Engineer!\n',
-            ]}
-            speed={99}
-            cursor={false}
-          />
+          <FadeIn>
+            I was previously a Game Designer at a local game company developing JRPG anime-style
+            mobile games for two years before transitioning into Software Engineering as a freelance
+            Backend Engineer!
+          </FadeIn>
           <br></br>
-          <TypeAnimation
-            style={{ whiteSpace: 'pre-line', display: 'block' }}
-            sequence={[
-              'I dabble in a bit of everything in both my professional and personal capacity, from backend, frontend to cloud, solution architecturing and DevOps, with a dash of MLOps in my current tenure at my full-time job. \n',
-            ]}
-            speed={99}
-            cursor={false}
-          />
+          <FadeIn>
+            I dabble in a bit of everything in both my professional and personal capacity, from
+            backend, frontend to cloud, solution architecturing and DevOps, with a dash of MLOps in
+            my current tenure at my full-time job.
+          </FadeIn>
         </p>
       </div>
     </div>
