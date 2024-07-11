5a4e47757a126ccfcf37858767456b0982ecb533
vi.channnnn <violet@himaa.me>
2024-07-11 22:28:15 +0900

    fix: switch to yarn

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,7 @@ WORKDIR /app
 
 ENV NODE_ENV=production
 
-COPY package.json package-lock.json ./
-RUN npm install --omit=dev --force
+COPY package.json yarn.lock ./
+RUN yarn install
 COPY . .
-RUN npm run build
+RUN yarn run build
--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 const withNextIntl = require('next-intl/plugin')();
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 /** @type {import('next').NextConfig} */
 const config = {
-  webpack: (config, options) => {
-    if (!config.resolve.plugins) {
-      config.resolve.plugins = [];
-    }
-    config.resolve.plugins.push(
-      new TsconfigPathsPlugin({
-        configFile: './tsconfig.json',
-      }),
-    );
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
@@ -24,21 +11,6 @@ const config = {
       },
     ],
   },
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: '/:path((?!ja|en|favicon.ico).*)',
-  //       destination: '/en',
-  //       permanent: true,
-  //       has: [
-  //         {
-  //           type: 'host',
-  //           value: '(.*?)',
-  //         },
-  //       ],
-  //     },
-  //   ];
-  // },
 };
 
 module.exports = withNextIntl(config);
